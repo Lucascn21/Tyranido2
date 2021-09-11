ec0c84d292bb2b58624e5100d07fbfb133a37a62
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2021-09-11 11:00:34 -0300

    Db now auto creates test account

--- helpers/database.js
+++ helpers/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const { monitorMongoose } = require("./mongooseHelpers");
+const userModel = require("../models/User");
+const { encrypt } = require("../helpers/handleBcrypt");
 
 const connectDB = async () => {
 	console.dir("Connecting to MongoDB");
@@ -7,6 +9,12 @@ const connectDB = async () => {
 		await mongoose.connect(process.env.DB_USERURI, {});
 		monitorMongoose(mongoose);
 		console.log("MongoDB connected!!");
+		const query = { username: "test1234" };
+		userModel.findOneAndUpdate(query, { username: "test1234", password:await encrypt("test1234", parseInt(process.env.HASH_SALT)),sessID:"0" } , {upsert: true, new: true, runValidators: true}, function (err, doc) {
+			if(err)console.dir(`error creando cuenta de prueba ${err}`);
+			if(doc)console.dir('datos de cuenta de prueba creados/actualizados con exito test1234:test1234');
+			
+		});
 	} catch (err) {
 		console.log("Failed to connect to MongoDB", err);
 		console.log("Check if MongoDB's service is running");
