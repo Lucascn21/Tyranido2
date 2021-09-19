cd475354b278490a011c80a886782d3655b205fe
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2021-09-19 11:18:51 -0300

    movie data to frontend

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -17,7 +17,7 @@ exports.search = async (req, res, next) => {
 				type: req.body.type, //	movie, series, episode, game
 				y: req.body.year,
 				page: req.body.page,
-				apikey: process.env.API_KEY ,
+				apikey: process.env.API_KEY,
 			},
 		})
 		.then(function (response) {
@@ -27,6 +27,19 @@ exports.search = async (req, res, next) => {
 				req.session.totalResults = response.data.totalResults;
 				req.session.searchResult.forEach((element) => {
 					if (element.Poster == "N/A") element.Poster = "/images/nopicture.png";
+					//Parsing ratings
+					if (element.Source == "Internet Movie Database") {
+						//Divido el string, en res[0] tengo el score actual, en res[1] tengo el valor total sobre el cual se divide
+						let res = element.Value.split("/");
+						element.Value = Math.round((res[0] / res[1]) * 100);
+					} else if (element.Source == "Rotten Tomatoes") {
+						let res = element.Value.split("%");
+						element.Value = parseInt(res[0]);
+					} else if (element.Source == "Metacritic") {
+						element.Value = parseInt(element.Value);
+					} else {
+						console.error("Unhandled rating: " + element.Value);
+					}
 				});
 			} else {
 				req.session.searchResult = [];
@@ -56,13 +69,13 @@ exports.search = async (req, res, next) => {
 				res.status(302);
 				req.session.message = `${resultsAmount} results out of ${totalResults}`;
 				req.session.alertType = "success";
-				return res.redirect("dashboard");
+				return res.redirect("lookup");
 			} else {
 				res.status(204);
 				req.session.message = "No results";
 				req.session.alertType = "warning";
 				req.session.searchResult = [];
-				return res.redirect("dashboard");
+				return res.redirect("lookup");
 			}
 		});
 };
